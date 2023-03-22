1324c0ef9f1c9c0936f82e5a658b8cc65d2edd10
Adriaan Dokter <adriaandokter@gmail.com>
2023-03-21 20:05:12 -0400

    splitting into separate files

--- macrodemography/R/macrodemography.R
+++ macrodemography/R/macrodemography.R
@@ -3,14 +3,12 @@
 #'
 #' @keywords internal
 #'
-#' @import parallel
+#' @import foreach
 #' @import assertthat
 #' @import sp
 #' @import rgee
+#' @importFrom brms bf brm as_draws_df prior
 #' @importFrom graphics lines points
-#' @importFrom stats median quantile sd weighted.mean
+#' @importFrom stats median quantile sd weighted.mean gaussian
+#' @importFrom utils capture.output setTxtProgressBar txtProgressBar
 "_PACKAGE"
-
-
-
-

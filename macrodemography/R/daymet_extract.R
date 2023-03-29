3502ba2c0553b96d1b1ca3dbda77a615711d5bfe
Adriaan Dokter <adriaandokter@gmail.com>
2023-03-28 21:19:18 -0400

    add weather regressions

--- macrodemography/R/daymet_extract.R
+++ macrodemography/R/daymet_extract.R
@@ -1,5 +1,4 @@
-#' extract Daymet summaries over a spatiotemporal cell of interest
-#' @import rgee 
+#' Extract Daymet summaries over a spatiotemporal cell of interest
 #' @param cell the cell id from a dggrid
 #' @param res the resolution of the dggrid
 #' @param variable the name of the daymet variable
@@ -28,3 +27,43 @@ daymet_extract <- function(cell = 621, res = 6, variable = "swe", mindate = "201
     )
   return(daymet$getInfo())
 }
+
+#' Extract a set of Daymet parameters for a cell and year
+#'
+#' Extracts a set of Daymet data according to a specification of variables, minimum and maximum dates
+#' using \link{daymet_extract}
+#' @param year the year of interest
+#' @param cell the large grid index
+#' @param grid a grid specification obtained via \link[dggridR]{dgconstruct}
+#' @param params_daymet a data.frame specifying daymet parameters and averaging periods
+#' @return a data.frame with cell-averaged daymet parameters
+#' @export
+#' @details
+#' Parameter `params_daymet` should be a `data.frame` with the following columns:
+#' \describe{
+#' \item{`label`}{a user-defined label to define the entry}
+#' \item{`variable`}{a valid Daymet variable}
+#' \item{`date_min`}{a minimum date formatted as mm-dd (without year)}
+#' \item{`date_max`}{a maximum date formatted as mm-dd (without year)}
+#' \item{`period`}{the corresponding demographic period (to associate a regression using \link{weather_regressions})}
+#' }
+daymet_set_extract <- function(year, cell, grid, params_daymet){
+  # initialize the data.frame for daymet data:
+  daymet_data=data.frame(matrix(nrow=1,ncol=nrow(params_daymet)+2,dimnames=list(NULL,c("cell","year",params_daymet$label))))
+  daymet_data$cell=cell
+  daymet_data$year=year
+  # fill data.frame with averaged daymet data:
+  for(lab in params_daymet$label){
+    # filter parameters to use for daymet extraction:
+    par_daymet <- params_daymet %>% filter(label==lab)
+    # construct min and max dates
+    maxdate=paste0(year,"-",par_daymet$date_max)
+    mindate=paste0(year,"-",par_daymet$date_min)
+    # address cases in which the min to max period includes a year change:
+    if(as.Date(maxdate)<as.Date(mindate)) mindate=paste0(year-1,"-",par_daymet$date_min)
+    # average over large cell, and store in data.frame
+    daymet_data[par_daymet$label] <- daymet_extract(cell=cell, res=grid$res, variable=par_daymet$variable, mindate=mindate, maxdate=maxdate)
+  }
+  return(daymet_data)
+}
+
--- macrodemography/R/weather_regressions.R
+++ macrodemography/R/weather_regressions.R
@@ -1,226 +1,113 @@
-#' perform weather regressions. currently expects to see data (and perform regressions)
-#' for winter temp, winter swe, and summer temp, but this would be easily updated by
-#' setting NULL defaults for these inputs plus a bit of control flow.
-#' @param cell_ratios cell ratios object
-#' @param cells_all all cells in cell_ratios
-#' @param years years underlying the cell ratios object
+#' Perform weather regressions.
+#'
+#' @param tidy_ratios ratios object, typically output of \link{make_ratios_tidy}
+#' @param data_daymet weather data, typically output of \link{daymet_set_extract}
+#' @param params_daymet data.frame specifying Daymet data extraction, see Details section of \link{daymet_set_extract}
 #' @param min_n minimum sample size for which to estimate a slope
-#' @param janfeb_temp jan/feb temps
-#' @param julaug_temp july/august temps
-#' @param decmar_swe dec-mar swe
-#' @return 2-element list.  prob_surv gives posterior probability that survival variance
-#'  is higher than productivity variance
-#'  coef is the average log-scale effect.
+#' @return a data.frame with regression results
 #' @export
-weather_regressions <- function(
-  cell_ratios, cells_all, years, min_n,
-  janfeb_temp, julaug_temp, decmar_swe
-){
-  plotting_data2 <- data.frame(
-    cell = cells_all,
-    surv_length = NA,
-    prod_length = NA,
-    full_length = NA,
-    janfeb_mean_bayes = NA,
-    janfeb_median_bayes = NA,
-    janfeb_sd_bayes = NA,
-    janfeb_skew_bayes = NA,
-    janfeb_kurt_bayes = NA,
-    janfeb_p_bayes = NA,
-    julaug_mean_bayes = NA,
-    julaug_median_bayes = NA,
-    julaug_sd_bayes = NA,
-    julaug_skew_bayes = NA,
-    julaug_kurt_bayes = NA,
-    julaug_p_bayes = NA,
-    swe_mean_bayes = NA,
-    swe_median_bayes = NA,
-    swe_sd_bayes = NA,
-    swe_skew_bayes = NA,
-    swe_kurt_bayes = NA,
-    swe_p_bayes = NA,
-    janfeb_flag = NA, julaug_flag = NA, swe_flag = NA
-  )
+#' @inheritParams compare_ratio_variances
+weather_regressions <- function(tidy_ratios, data_daymet, params_daymet, min_n, warmup=1000, iter=2000, chains=3, quiet=FALSE){
 
-  cell_ratio_series <- cell_ratios$summary
-  cell_ratio_series_full <- cell_ratios$replicates
+  #initialize output data.frame
+  df_out <- data.frame()
+
+  tidy_ratio_series <- tidy_ratios$summary %>%
+    filter(cell != 233) %>% # cell 233 is out over open ocean
+    left_join(data_daymet, by=c("cell","year")) # add weather data
+
+  years=unique(tidy_ratios$summary$year)
   years_len <- length(years) - 1
+  cells_all <- sort(unique(tidy_ratios$summary$cell))
 
-  pb <- txtProgressBar(min = 1, max = length(cells_all), initial = 1)
-  for (i in seq_along(cells_all)) {
-    setTxtProgressBar(pb, i)
-    if(i == 233){next} # This is a cell out over open ocean
-    if (!identical(cell_ratio_series[[i]], NA)) {
-      crsi <- cell_ratio_series[[i]]
-      plotting_data2$surv_length[i] <- sum(is.finite(crsi$median[2*seq_len(years_len)]))
-      plotting_data2$prod_length[i] <- sum(is.finite(crsi$median[2*seq_len(years_len + 1) - 1]))
-      plotting_data2$full_length[i] <- sum(is.finite(crsi$median[1 + 2*seq_len(years_len)] + crsi$median[2*seq_len(years_len)]))
+  # initiate a progress bar when quiet = T
+  if(quiet) pb <- txtProgressBar(min = 1, max = length(cells_all), initial = 1)
 
-      janfeb_temp_slopes <- julaug_temp_slopes <- janfeb_swe_slopes <- vector()
+  for (i in seq_along(cells_all)){
+    # update the progress bar
+    if(quiet) setTxtProgressBar(pb, i)
+    # select data for a single cell
+    # crsi stands for 'cell ratio series i'
 
-      lrat_means <- rowMeans(cell_ratio_series_full[[i]])
-      lrat_sds <- apply(cell_ratio_series_full[[i]], 1, sd)
+    adapt_delta <- 0.8
+    converged <- FALSE
+    tries <- 0
 
-      surv_means <- prod_means <- lrat_means
-      surv_sds <- prod_sds <- lrat_sds
-      surv_means[!use_cell_years(cell_ratio_series[[i]],
-                                 inf_exclude = F,
-                                 n_min_prod = 0,
-                                 n_min_surv = min_n,
-                                 n_min_full = 0)] <- NA
-      surv_means <- surv_means[2*c(1:13)]
-      surv_sds[!use_cell_years(cell_ratio_series[[i]],
-                               inf_exclude = F,
-                               n_min_prod = 0,
-                               n_min_surv = min_n,
-                               n_min_full = 0)] <- NA
-      surv_sds <- surv_sds[2*seq_len(years_len)]
+    for(j in seq_along(nrow(params_daymet))){
+      # the daymet parameter to use in regression:
+      par = params_daymet$label[j]
+      # the period (demographic index) to use in regression:
+      period_demographic = params_daymet$period[j]
+      # select the data for the regression:
+      data_regression <- tidy_ratio_series %>%
+        filter(cell==cells_all[i]) %>%
+        filter(period==period_demographic) %>%
+        filter(!is.na(avg))
+      # initialize output
+      output=data.frame(period=period_demographic,label=par,n=nrow(data_regression),
+                        mean=NA,median=NA,sd=NA,skewness=NA,kurtosis=NA,p_value=NA,converged=NA)
 
-      prod_means[!use_cell_years(cell_ratio_series[[i]],
-                                 inf_exclude = F,
-                                 n_min_prod = min_n,
-                                 n_min_surv = 0,
-                                 n_min_full = 0)] <- NA
-      prod_means <- prod_means[1 + 2*c(0:13)]
-      prod_sds[!use_cell_years(cell_ratio_series[[i]],
-                               inf_exclude = F,
-                               n_min_prod = min_n,
-                               n_min_surv = 0,
-                               n_min_full = 0)] <- NA
-      prod_sds <- prod_sds[2*seq_len(years_len + 1) - 1]
+      if(nrow(data_regression)>min_n){
+        # add column `response` containing a copy of the data we want to regress:
+        data_regression$predictor=data_regression[[par]]
 
-      if (sum(!is.na(surv_means)) > min_n) {
-        surv_df <- data.frame(mean = surv_means, sd = surv_sds,
-                              janfeb_temp = unlist(janfeb_temp[[i]][1:13]),
-                              decmar_swe = sqrt(unlist(decmar_swe[[i]][1:13])))
-        capture.output(suppressWarnings(suppressMessages(
-          janfeb_mod <- brm(bf(mean | resp_se(sd, sigma = TRUE) ~ janfeb_temp),
-                          data = surv_df, family = gaussian(),
-                          prior = prior(std_normal(), class = "b"),
-                          iter = 2000, warmup = 1000, chains = 3, refresh = 0,
-                          backend = "cmdstanr")
-        )))
-        diagnostics <- check_brmsfit_diagnostics(janfeb_mod)
-        if (!all(diagnostics)) {
-          adapt_delta <- .8
-          iter <- 2000
+        # construct brms regression formula:
+        brms_formula <- bf(avg | resp_se(sd, sigma = TRUE) ~ predictor)
+
+        while(!converged & tries < 2){
+
+          if(!quiet) print(paste("starting estimation for cell",cells_all[i],"and parameter", par))
+
+          # Here we hard-code that in addition to meeting the thresholds for inclusion,
+          # we also need to see at least two years with nonzero snow.
+          if(par=="swe" & (sum((data_regression$predictor > 0) & (!is.na(data_regression$avg))) > 2)) break
+
+          # run brms model
+          capture.output(suppressWarnings(suppressMessages(
+            mod <- brm(brms_formula, data = data_regression, family = gaussian(),
+                       prior = prior(std_normal(), class = "b"),
+                       ter = iter, warmup = warmup, chains = chains, refresh = 0,
+                       backend = "cmdstanr", silent=ifelse(quiet,2,1))
+          )))
+
+          diagnostics <- check_brmsfit_diagnostics(mod)
+          if(all(diagnostics)) converged = TRUE
+
           if (!diagnostics[1]) {
             adapt_delta <- .99
           }
           if (!diagnostics[2]) {
-            iter <- janfeb_iter[i] <- 4000
+            iter <- iter*2
           }
-          capture.output(suppressWarnings(suppressMessages(
-            janfeb_mod <- brm(bf(mean | resp_se(sd, sigma = TRUE) ~ janfeb_temp),
-                            data = surv_df, family = gaussian(),
-                            prior = prior(std_normal(), class = "b"),
-                            iter = iter, warmup = 1000, adapt_delta = adapt_delta,
-                            chains = 3, refresh = 0,
-                            backend = "cmdstanr")
-          )))
-          diagnostics <- check_brmsfit_diagnostics(janfeb_mod)
-          if(!all(diagnostics)){
-            plotting_data2$janfeb_flag[i] <- TRUE
-          }
+
+          tries = tries + 1
+
+        } # while
+
+        if(converged){
+          # sample the posterior distribution:
+          d <- as_draws_df(mod)
+
+          slopes <- d$b_predictor
+
+          output=data.frame(period=period_demographic,
+                            label=par,
+                            n=nrow(data_regression),
+                            mean=mean(slopes),
+                            median=median(slopes),
+                            sd=sd(slopes),
+                            skewness=moments::skewness(slopes),
+                            kurtosis=moments::kurtosis(slopes),
+                            p_value=mean(slopes > 0),
+                            converged=converged
+          )
+        } else{
+          output$converged=FALSE
+          if(!quiet) print(paste("diagnostic failure for cell", cell_index))
         }
-
-        janfeb_temp_slopes <- as_draws_df(janfeb_mod)$b_janfeb_temp
-
-        plotting_data2$janfeb_mean_bayes[i] <- mean(janfeb_temp_slopes)
-        plotting_data2$janfeb_median_bayes[i] <- median(janfeb_temp_slopes)
-        plotting_data2$janfeb_sd_bayes[i] <- sd(janfeb_temp_slopes)
-        plotting_data2$janfeb_skew_bayes[i] <- moments::skewness(janfeb_temp_slopes)
-        plotting_data2$janfeb_kurt_bayes[i] <- moments::kurtosis(janfeb_temp_slopes)
-        plotting_data2$janfeb_p_bayes[i] <- mean(janfeb_temp_slopes > 0)
-
-        # Here we hard-code that in addition to meeting the thresholds for inclusion,
-        # we also need to see at least two years with nonzero snow.
-        if (sum((surv_df$decmar_swe > 0) & (!is.na(surv_df$mean))) > 2) {
-          capture.output(suppressWarnings(suppressMessages(
-            swe_mod <- brms::brm(bf(mean | resp_se(sd, sigma = TRUE) ~ decmar_swe),
-                               prior = prior(std_normal(), class = "b"),
-                               data = surv_df, family = gaussian(),
-                               iter = 2000, warmup = 1000, chains = 3, refresh = 0,
-                               backend = "cmdstanr")
-          )))
-          diagnostics <- check_brmsfit_diagnostics(swe_mod)
-          if (!all(diagnostics)) {
-            adapt_delta <- .8
-            iter <- 2000
-            if (!diagnostics[1]) {
-              adapt_delta <- .99
-            }
-            if (!diagnostics[2]) {
-              iter <- swe_iter[i] <- 4000
-            }
-            capture.output(suppressWarnings(suppressMessages(
-              swe_mod <- brm(bf(mean | resp_se(sd, sigma = TRUE) ~ decmar_swe),
-                           data = surv_df, family = gaussian(),
-                           prior = prior(std_normal(), class = "b"),
-                           iter = iter, warmup = 1000, adapt_delta = adapt_delta,
-                           chains = 3, refresh = 0,
-                           backend = "cmdstanr")
-            )))
-            diagnostics <- check_brmsfit_diagnostics(swe_mod)
-            if(!all(diagnostics)){
-              plotting_data2$swe_flag[i] <- TRUE
-            }
-          }
-
-          swe_slopes <- as_draws_df(swe_mod)$b_decmar_swe
-
-          plotting_data2$swe_mean_bayes[i] <- mean(swe_slopes)
-          plotting_data2$swe_median_bayes[i] <- median(swe_slopes)
-          plotting_data2$swe_sd_bayes[i] <- sd(swe_slopes)
-          plotting_data2$swe_skew_bayes[i] <- moments::skewness(swe_slopes)
-          plotting_data2$swe_kurt_bayes[i] <- moments::kurtosis(swe_slopes)
-          plotting_data2$swe_p_bayes[i] <- mean(swe_slopes > 0)
-        }
-      }
-
-      if (sum(!is.na(prod_means)) > min_n) {
-        prod_df <- data.frame(mean = prod_means, sd = prod_sds,
-                              julaug_temp = unlist(julaug_temp[[i]]))
-        capture.output(suppressWarnings(suppressMessages(
-          julaug_mod <- brms::brm(bf(mean | resp_se(sd, sigma = TRUE) ~ julaug_temp),
-                                data = prod_df, family = gaussian(),
-                                prior = prior(std_normal(), class = "b"),
-                                iter = 2000, warmup = 1000, chains = 3, refresh = 0,
-                                backend = "cmdstanr")
-        )))
-        diagnostics <- check_brmsfit_diagnostics(julaug_mod)
-        if (!all(diagnostics)) {
-          adapt_delta <- .8
-          iter <- 2000
-          if (!diagnostics[1]) {
-            adapt_delta <- .99
-          }
-          if (!diagnostics[2]) {
-            iter <- julaug_iter[i] <- 4000
-          }
-          capture.output(suppressWarnings(suppressMessages(
-            julaug_mod <- brm(bf(mean | resp_se(sd, sigma = TRUE) ~ julaug_temp),
-                            data = surv_df, family = gaussian(),
-                            prior = prior(std_normal(), class = "b"),
-                            iter = iter, warmup = 1000, adapt_delta = adapt_delta,
-                            chains = 3, refresh = 0,
-                            backend = "cmdstanr")
-          )))
-          diagnostics <- check_brmsfit_diagnostics(julaug_mod)
-          if(!all(diagnostics)){
-            plotting_data2$julaug_flag[i] <- TRUE
-          }
-        }
-        julaug_temp_slopes <- as_draws_df(julaug_mod)$b_julaug_temp
-
-        plotting_data2$julaug_mean_bayes[i] <- mean(julaug_temp_slopes)
-        plotting_data2$julaug_median_bayes[i] <- median(julaug_temp_slopes)
-        plotting_data2$julaug_sd_bayes[i] <- sd(julaug_temp_slopes)
-        plotting_data2$julaug_skew_bayes[i] <- moments::skewness(julaug_temp_slopes)
-        plotting_data2$julaug_kurt_bayes[i] <- moments::kurtosis(julaug_temp_slopes)
-        plotting_data2$julaug_p_bayes[i] <- mean(julaug_temp_slopes > 0)
-      }
-    }
-  }
-  plotting_data2
+      } # if
+    df_out <- rbind(df_out,output)
+    } # for loop across daymet parameters
+  } # for loop across cells
+  df_out
 }
